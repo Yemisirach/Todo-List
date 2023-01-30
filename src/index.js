05137af50604e8d2e6163d3ba218105a9d852bf4
Yemisirach <yemsrach3723@gmail.com>
2023-01-31 02:05:05 +0300

    complet add and remove functionality

--- src/index.js
+++ src/index.js
@@ -1,114 +1,122 @@
-import './style.css';
+const addButton = document.getElementById("add");
+const input = document.getElementById("todo");
+const listContainer = document.getElementById("task-list");
+const clearAll = document.getElementById("clearAll");
+let newTodo = "";
+const toDoList = [];
 
-const todoInput = document.querySelector('.todo-input input');
-const taskBox = document.querySelector('.task-box');
-const clearAll = document.querySelector('.clear');
-const filters = document.querySelectorAll('.filters span');
-let editId;
-let isEditTask = false;
-let todos = JSON.parse(localStorage.getItem('todo-list'));
-
-function showTodo(filter) {
-  let liTag = '';
-  if (todos) {
-    todos.forEach((todo, id) => {
-      const completed = todo.completed === 'completed' ? 'checked' : '';
-      if (filter === todo.completed || filter === 'all') {
-        liTag += `<li class="task">
-        <article class="task-manager">
-                          <label for="${id}">
-                              <input onclick="updatestatus(this)" type="checkbox" id="${id}" ${completed}>
-                              <p class="${completed}">${todo.name}</p>
-                          </label>
-                          <div class="menu">
-                              <i onclick="showMenu(this)" id="showMenu" class="uil uil-ellipsis-h"></i>
-                              <ul class="task-menu">
-                                  <li class="editTask" onclick="editTask(${id}, "${todo.name}")"><i class="uil uil-pen"></i></li>
-                                  <li class="deleteTask" onclick="deleteTask(${id}, "${filter}")"><i class="uil uil-trash"></i></li>
-                              </ul>
-                          </div>
-                      </li>
-                      </article>
-                           <hr>`;
-      }
-    });
-  }
-  taskBox.innerHTML = liTag;
-}
-showTodo();
-
-filters.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    document.querySelector('span.active').classList.remove('active');
-    btn.classList.add('active');
-    showTodo(btn.id);
+const handleCheckbox = (e) => {
+  index = Number(e.target.parentNode.getAttribute("id"));
+  console.log(index);
+  toDoList[index - 1].completed = !toDoList[index - 1].completed;
+  console.log(toDoList);
+  localStorage.setItem("taskList", JSON.stringify(toDoList));
+};
+const deleteTask = (e) => {
+  console.log(toDoList);
+  const task = e.target.parentNode;
+  const li = task.querySelector("li");
+  const taskList = document.getElementById("task-list");
+  taskList.removeChild(task);
+  console.log(Number(li.getAttribute("id")));
+  toDoList.splice(Number(li.getAttribute("id") - 1), 1);
+  toDoList.forEach((task, index) => {
+    document.getElementById(task.index).setAttribute("id", index + 1);
+    task.index = index + 1;
   });
-});
-clearAll.addEventListener('click', () => {
-  isEditTask = false;
-  todos.splice(0, todos.length);
-  localStorage.setItem('todo-list', JSON.stringify(todos));
-  showTodo();
-});
+  localStorage.setItem("taskList", JSON.stringify(toDoList));
+  console.log(toDoList);
+};
 
-todoInput.addEventListener('keyup', (e) => {
-  const userTask = todoInput.value.trim();
-  if (e.key === 'Enter' && userTask) {
-    if (!isEditTask) {
-      todos = !todos ? [] : todos;
-      const taskInfo = { name: userTask, completed: 'false', index: 0 };
-      todos.push(taskInfo);
-    } else {
-      isEditTask = false;
-      todos[editId].name = userTask;
-    }
-    todoInput.value = '';
-    localStorage.setItem('todo-list', JSON.stringify(todos));
-    showTodo(document.querySelector('span.active').id);
-  }
-});
+const saveEditTask = (e) => {
+  const li = e.target.parentNode.parentNode;
+  const target = li.querySelector("#save");
+  const input = li.querySelector('input[type="text"]');
+  const text = input.value;
+  const P = document.createElement("p");
+  P.textContent = text;
+  console.log(li);
+  li.replaceChild(P, target);
+  toDoList.filter((task) => task.index === Number(li.id))[0].name = text;
+  console.log(toDoList);
+  localStorage.setItem("taskList", JSON.stringify(toDoList));
+};
 
-const showMenu = document.querySelector('#showMenu');
-
-showMenu.addEventListener('click', (selectedTask) => {
-  const menuDiv = selectedTask.parentElement.lastElementChild;
-  menuDiv.classList.add('show');
-  document.addEventListener('click', (e) => {
-    if (e.target.tagName !== 'I' || e.target !== selectedTask) {
-      menuDiv.classList.remove('show');
-    }
+const editTask = (e) => {
+  const task = e.target.parentNode;
+  const p = task.querySelector("p");
+  const li = task.querySelector("li");
+  const text = p.textContent;
+  const input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.value = text;
+  input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") saveEditTask(e);
   });
-});
+  const saveButton = document.createElement("button");
+  saveButton.textContent = "Save";
+  saveButton.addEventListener("click", saveEditTask);
+  const div = document.createElement("div");
+  div.setAttribute("id", "save");
+  div.appendChild(input);
+  div.appendChild(saveButton);
+  li.replaceChild(div, p);
+  localStorage.setItem("taskList", JSON.stringify(toDoList));
+};
 
-const updatestatus = document.querySelector('refreshe');
-
-updatestatus.addEventListener('click', (selectedTask) => {
-  const taskName = selectedTask.parentElement.lastElementChild;
-  if (selectedTask.checked) {
-    taskName.classList.add('checked');
-    todos[selectedTask.id].completed = 'completed';
-  } else {
-    taskName.classList.remove('checked');
-    todos[selectedTask.id].completed = 'pending';
+const handleAddTask = (name = "", completed = false, newTask = true) => {
+  if (!name) return;
+  const index = toDoList.length + 1;
+  toDoList.push({ name, completed, index });
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", deleteTask);
+  const editButton = document.createElement("button");
+  editButton.textContent = "Edit";
+  editButton.addEventListener("click", editTask);
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.checked = completed;
+  checkbox.addEventListener("change", handleCheckbox);
+  const task = document.createElement("div");
+  const li = document.createElement("li");
+  li.setAttribute("id", toDoList.length);
+  const p = document.createElement("p");
+  p.textContent = name;
+  li.appendChild(checkbox);
+  li.appendChild(p);
+  task.appendChild(li);
+  task.appendChild(deleteButton);
+  task.appendChild(editButton);
+  listContainer.appendChild(task);
+  input.value = "";
+  newTodo = "";
+  if (newTask) {
+    localStorage.setItem("taskList", JSON.stringify(toDoList));
   }
-  localStorage.setItem('todo-list', JSON.stringify(todos));
+};
+
+// this is were the java script code sarts to execute
+
+input.addEventListener("input", (e) => {
+  newTodo = e.target.value;
 });
 
-const editTask = document.querySelector('.editTask');
+addButton.addEventListener("click", (e) => handleAddTask(newTodo));
 
-editTask.addEventListener('click', (taskId, textName) => {
-  editId = taskId;
-  isEditTask = true;
-  todoInput.value = textName;
-  todoInput.focus();
-  todoInput.classList.add('active');
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") handleAddTask(newTodo);
 });
 
-const deleteTask = document.querySelector('.deleteTask');
-
-deleteTask.addEventListener('click', (deleteId, filter) => {
-  isEditTask = false;
-  todos.splice(deleteId, 1);
-  localStorage.setItem('todo-list', JSON.stringify(todos));
-  showTodo(filter);
+clearAll.addEventListener("click", (e) => {
+  localStorage.setItem("taskList", JSON.stringify([]));
+  listContainer.innerHTML = "";
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const initialList = localStorage.getItem("taskList")
+    ? JSON.parse(localStorage.getItem("taskList"))
+    : [];
+  initialList.forEach((task) =>
+    handleAddTask(task.name, task.completed, false)
+  );
 });
